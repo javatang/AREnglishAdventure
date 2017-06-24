156a1146ee7b9499834c650cebcf5fc2551eecc8
Xinhou Jiang <919575700@qq.com>
2017-06-24 19:31:13 +0800

    AR界面配置以及基本UI；

---
+++ Assets/Scripts/Commen/TrackableImageTarget.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Vuforia{
+	public class TrackableImageTarget : DefaultTrackableEventHandler {
+		public GameObject ARObject;
+		public override void OnARObjectFound(){
+			ARObject.SetActive (true);
+		}
+
+		public override void OnARObjectLost(){
+			ARObject.SetActive (false);
+		}
+	}
+}
