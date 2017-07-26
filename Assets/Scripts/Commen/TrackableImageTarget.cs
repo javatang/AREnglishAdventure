6bf912072e75122af4a24a12c9dee84d76b1718e
Xinhou Jiang <919575700@qq.com>
2017-07-26 11:04:52 +0800

    ARPLant&ARZoo

--- Assets/Scripts/Commen/TrackableImageTarget.cs
+++ Assets/Scripts/Commen/TrackableImageTarget.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 namespace Vuforia{
 	public class TrackableImageTarget : DefaultTrackableEventHandler {
-		public GameObject ARObject;
+		public int index = 0;        // imagetarget数组下标
+		public string objectName;    // 模型名字（语音文字）
+		public string obejctComment; // 中英文字注释
+		public GameObject ARObject;  // 要显示的VR模型
+
 		public override void OnARObjectFound(){
 			ARObject.SetActive (true);
+			ARManager.Ins.curImageTarget = gameObject;
+			ARManager.Ins.UpdateARModel ();
 		}
 
 		public override void OnARObjectLost(){
---
+++ Assets/Scripts/GameMap/ARManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Vuforia {
+	public class ARManager : Singleton<ARManager> {
+
+		public Text dialogText;
+		public GameObject curImageTarget;
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+
+		public void OnHome(){
+			UIManager.Ins.EnterLoadingScene ("NavMap");
+		}
+
+		public void OnAction(){
+			UpdateARModel();
+		}
+
+		public void UpdateARModel() {
+			// 说话
+			TrackableImageTarget curImgTarget = curImageTarget.GetComponent<TrackableImageTarget>();
+			dialogText.text = curImgTarget.obejctComment;
+			UIManager.Ins.UnityState (curImgTarget.objectName);
+		}
+	}
+}
