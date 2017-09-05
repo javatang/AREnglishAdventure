0323a90a477aa5c363e10bdffbec6baff0f6021f
Xinhou Jiang <919575700@qq.com>
2017-09-06 00:14:53 +0800

    更新；

--- Assets/Scripts/GameMap/ARFightManager.cs
+++ Assets/Scripts/GameMap/ARFightManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ARFightManager : Singleton<ARFightManager> {
 	public GameObject bulletPrefab;
+	public GameObject explodePrefab;
 	public GameObject target;
 	public Animator animator;
 	public LineRenderer linerender;
+
+	private int action = 0;
 	// Use this for initialization
 	void Start () {
 	}
@@ -14,7 +17,7 @@ public class ARFightManager : Singleton<ARFightManager> {
 	// Update is called once per frame
 	void Update () {
 		linerender.SetPosition (0, Camera.main.transform.position);
-		linerender.SetPosition (1, target.transform.position);
+		linerender.SetPosition (1, Vector3.zero);
 
 		if(Input.GetMouseButtonDown(0)){
 		}
@@ -28,13 +31,16 @@ public class ARFightManager : Singleton<ARFightManager> {
 		animator.SetInteger ("Action", action);
 	}
 
-	public void OnHome(){
+	public void OnHome() {
 		UIManager.Ins.EnterLoadingScene ("NavMap");
 	}
 
 	public void OnShot() {
 		GameObject bullet = GameObject.Instantiate (bulletPrefab, Camera.main.transform.position, Camera.main.transform.rotation) as GameObject;
+		GameObject explode = GameObject.Instantiate (explodePrefab, target.transform.position + new Vector3(0,1,0), target.transform.rotation) as GameObject;
 		UIManager.Ins.PlaySoundEffect (4);
+		++action;
+		SetAction (action%7+1);
 	}
 
 	public void Pronunication() {
