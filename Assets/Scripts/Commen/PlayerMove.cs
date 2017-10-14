315b854bb2198fac26105dbd2cb164c87feb22d0
Xinhou Jiang <919575700@qq.com>
2017-10-14 18:06:24 +0800

    选卡界面；

---
+++ Assets/Scripts/Commen/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float m_speed = 1f;
+	bool isWalking = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isWalking){
+			MoveForward();
+		}
+	}
+	public void ToogleWalking(){
+		isWalking = !isWalking;
+	}
+
+	public void MoveForward(){
+		this.transform.Translate(Vector3.forward*m_speed*Time.deltaTime);
+	}
+	public void MoveLeft(){
+		this.transform.Translate(Vector3.right*-m_speed*Time.deltaTime);
+	}
+	public void MoveRight(){
+		this.transform.Translate(Vector3.right*m_speed*Time.deltaTime);
+	}
+	public void MoveBack(){
+		this.transform.Translate(Vector3.forward*-m_speed*Time.deltaTime);
+	}
+}
--- Assets/Scripts/GameMap/NavMapManager.cs
+++ Assets/Scripts/GameMap/NavMapManager.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NavMapManager : MonoBehaviour {
+	
+	public Material[] skyboxs;
 
+	int frame = 0;
+	int maxframe = 100;
+	int index = 0;
+	int maxindex=0;
 	// Use this for initialization
 	void Start () {
 		// 背景音乐
-		UIManager.Ins.PlayBgMusic(1);
+		//UIManager.Ins.PlayBgMusic(1);
+		maxindex = skyboxs.Length;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		frame++;
+		if (frame > maxframe) {
+			Camera.main.GetComponent<Skybox>().material = skyboxs[index%maxindex];
+			index++;
+			frame = 0;
+		}
 	}
 
 	// 进入AR战斗场景
@@ -27,12 +39,12 @@ public class NavMapManager : MonoBehaviour {
 
 	// 植物园
 	public void ARZoo () {
-		UIManager.Ins.EnterLoadingScene ("ARZoo");
+		UIManager.Ins.EnterLoadingScene ("MenuZoo");
 	}
 
 	// 动物园
 	public void ARPlant () {
-		UIManager.Ins.EnterLoadingScene ("ARPlant");
+		UIManager.Ins.EnterLoadingScene ("MenuPlant");
 	}
 
 	// LBS
